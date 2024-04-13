f6c5fa2b80b54b3b046f60c3ab6cc760bb6f72de
Rajan Walia <rajanwalia4@gmail.com>
2024-04-13 13:10:11 +0530

    Added time module

---
+++ 05time/go.mod
@@ -0,0 +1,5 @@
+module time
+
+go 1.20
+
+// require golang.org/x/time v0.5.0 // indirect
---
+++ 05time/main.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func main() {
+	fmt.Println("Welcome to time of Golang")
+
+	currentTime := time.Now()
+	fmt.Println(currentTime)
+}
