76b82d1736be8c3eaf1f1794d10ae7d09a5b1e16
Rajan Walia <rajanwalia4@gmail.com>
2024-05-14 00:12:39 +0530

    added a simple conditional program

--- 03userinput/main.go
+++ 03userinput/main.go
@@ -3,7 +3,10 @@ package main
 import (
 	"bufio"
 	"fmt"
+	"log"
 	"os"
+	"strconv"
+	"strings"
 )
 
 func main() {
@@ -13,8 +16,18 @@ func main() {
 	// comma ok || err ok - syntax also kind of try catch
 
 	//  The following syntax will always return string
-	input, _ := reader.ReadString('\n') // read till '\n' comes
+	input, error := reader.ReadString('\n') // read till '\n' comes
+	if error != nil {
+		log.Fatal(error)
+	}
+	input = strings.TrimSpace(input)
+	grade, error := strconv.ParseFloat(input, 64)
+	if grade >= 60 {
+		fmt.Println("Pass")
+	} else {
+		fmt.Println("Fail")
+	}
 
-	fmt.Println("Hello there ", input)
-	fmt.Printf("Type of input is %T", input)
+	// fmt.Println("Hello there ", input)
+	// fmt.Printf("Type of input is %T", input)
 }
